95c1a658c1168453aa5c6aed8799916423ed4d7b
miftanurfarid <miftanurfarid@gmail.com>
2020-04-10 09:29:03 +0800

    tambah load data atenuasi

--- code/cal_Nf.m
+++ code/cal_Nf.m
@@ -1,9 +1,13 @@
-function Nf = cal_Nf(P1000, Pf)
+function cal_Nf(P1000, Pf)
     % Fungsi untuk menghitung atenuasi relatif terhadap
     % atenuasi frekuensi 1000 Hz.
     % Persamaan yang digunakan berdasarkan buku 
     % Computer & Data Communication 10th Edition - William Stallings
 
-    Nf = (-10).*log10(Pf ./ P1000);
-    
+    f = Pf;
+    data = load("data.dat");
+    locRef = find(data(1,:) == P1000);
+    locF = find(data(1,:) == f);
+    Nf = (-10).*log10(data(1,locF(1)) ./ data(1,locRef(1)));
+    fprintf('%.4f dB\n', Nf);
 end
