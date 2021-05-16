eb922fbf3a6bb8bc9d940074b1dc5dfd401c29d2
miftanurfarid <miftanurfarid@gmail.com>
2021-05-17 01:34:48 +0800

    soal kuis 2 2020 2021

---
+++ soal/kuis/kuis_2_komunikasi_data_2020_2021.m
@@ -0,0 +1,23 @@
+% Teknik Pengkodean Data Analog Sinyal Digital
+% Soal 1 
+t = 0:0.1:0.7;
+f = 16000;
+y = 15*abs(sind(2*pi*f*t));
+figure(1);
+plot(t, y, 'linewidth', 2);
+grid on;
+set (gca, "yminorgrid", "on");
+ylim( [ 0 17 ] )
+xlabel("Time (second)");
+ylabel("Normalized magnitude");
+
+% Soal 2
+
+t = 0:0.1:2;
+f = 16000;
+y = abs(sind(2*pi*f*t)+sind(2*pi*0.5*f*t));
+figure(2);
+plot(t, y, 'linewidth', 2);
+grid minor;
+xlabel("Time (second)");
+ylabel("Signal amplitude");
